7bfe1172cf2cd78671e8419289872551ce4541b7
Junho Eum <jxe282@gmail.com>
2023-08-06 16:12:30 -0700

    Update 3DHTML.jsx

--- src/components/3DHTML.jsx
+++ src/components/3DHTML.jsx
@@ -1,17 +1,18 @@
 function HtmlComp() {
   return (
     <>
-    <p className='figure-description'>Use your mouse to rotate the 3D plot. Scroll to zoom in and out.</p>
-    <div className="container-3dPlot left-side">
-      <iframe
-        src="/3d-plot.html"
-        title="3D Plot"
-        width="100%"
-        height="100%"
-        
-        style={{ border: "none" }}
-      />
-    </div>
+      <div className="container-3dPlot left-side">
+        <p className="figure-description">
+          Use your mouse to rotate the 3D plot. Scroll to zoom in and out.
+        </p>
+        <iframe
+          src="/3d-plot.html"
+          title="3D Plot"
+          width="100%"
+          height="100%"
+          style={{ border: "none", background: "white" }} // Set the background to white here
+        />
+      </div>
     </>
   );
 }
