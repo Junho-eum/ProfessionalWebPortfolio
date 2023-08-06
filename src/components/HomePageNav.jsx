e88f644dac46cee6ae61816d184d521912e30108
Junho Eum <jxe282@gmail.com>
2023-08-05 21:40:05 -0700

    Add files via upload

---
+++ src/components/HomePageNav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles/HomePageNav.css";
+
+function HomePageNav() {
+    return (
+<ul className="NavulList">
+  <li className="NavList">Lrm</li>
+  <li className="NavList">Mollitia</li>
+  <li className="NavList">Commod!</li>
+  <li className="NavList" >Dolorum?</li>
+  <li className="NavList">Soluta.</li>
+</ul>
+    )
+}
+
+export default HomePageNav;
