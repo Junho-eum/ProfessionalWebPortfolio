334d7a491fbb23b3e0b5d9f606d8c752ba84dc76
Junho Eum <jxe282@gmail.com>
2023-08-06 20:36:39 -0700

    Update HomePageNav.jsx

--- src/components/HomePageNav.jsx
+++ src/components/HomePageNav.jsx
@@ -1,17 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import "./styles/HomePageNav.css";
 
+
 function HomePageNav() {
-    return (
-        <ul className="NavulList">
-            <li className="NavList">About</li>
-            <li className="NavList">
-                <a href="https://github.com/Junho-eum/Baseball_Analytics/blob/main/KBO_analytics_draft3.pdf" target="_blank" rel="noopener noreferrer">
-                    Github
-                </a>
-            </li>
-        </ul>
-    );
+  return (
+    <ul className="NavulList">
+      <li className="NavList">
+        <Link to="/article">About</Link>
+      </li>
+      <li className="NavList">
+        <a
+          href="https://github.com/Junho-eum/Baseball_Analytics/blob/main/KBO_analytics_draft3.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+      </li>
+    </ul>
+  );
 }
 
 export default HomePageNav;
