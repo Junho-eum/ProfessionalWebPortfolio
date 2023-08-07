aca1be86ad1ef6b961b358a5b8621d2213124c6b
Junho Eum <jxe282@gmail.com>
2023-08-06 20:41:53 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Page1 from "./pages/HomePage";
 import Page2 from "./pages/PageTwo";
 import Page3 from "./pages/PageThree";
 import Page4 from "./pages/PageFour";
-import ArticlePage from "./pages/ArticlePage"; // Import ArticlePage
+
 
 import "./App.css";
 
@@ -17,7 +17,7 @@ function App() {
           <Route path="page2" element={<Page4 />} />
           <Route path="page3" element={<Page2 />} />
           <Route path="page4" element={<Page4 />} />
-          <Route path="article" element={<ArticlePage />} /> {/* New route */}
+          
         </Routes>
       </div>
     </Router>
