6b3c07af97f24c31995b926b1beec514b88f3fa6
Junho Eum <jxe282@gmail.com>
2023-08-06 20:45:34 -0700

    Add files via upload

---
+++ src/pages/ArticlePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./ArticlePage.css"; // assuming the CSS is defined in this file
+
+const ArticlePage = () => {
+  return (
+    <div className="my-custom-body">
+      <header>header</header>
+      <nav>nav</nav>
+      <section>section</section>
+      <aside>aside</aside>
+      <footer>footer</footer>
+    </div>
+  );
+};
+
+export default ArticlePage;
