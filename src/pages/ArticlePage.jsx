4399df5a07d7d5fbf571a4a983c2b17d6215b37e
Junho Eum <jxe282@gmail.com>
2023-08-08 15:43:42 -0700

    Update ArticlePage.jsx

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useRef } from "react";
 import VerticalMenu from "../components/VerticalMenu"; // Adjust the path as needed
 import "../components/styles/ArticlePage.css";
+import FilterTab from "../components/FilterTab";
 
 function ArticlePage() {
   const [activeSection, setActiveSection] = useState(0);
-  const sectionRefs = [useRef(null), useRef(null), useRef(null)];
+  const backgroundColors = ["white", "white", "white"]; // Define the background colors
 
   const handleSectionChange = (index) => {
     setActiveSection(index);
@@ -17,42 +18,34 @@ function ArticlePage() {
         <VerticalMenu onSectionChange={handleSectionChange} />
       </nav>
       <section className="article-section">
-        <div
-          ref={sectionRefs[0]}
-          style={
-            activeSection === 0
-              ? { backgroundColor: "red", fontSize: "larger" }
-              : { backgroundColor: "red" }
-          }
-        >
-          Content for section 1 (CV)
-        </div>
-        <div
-          ref={sectionRefs[1]}
-          style={
-            activeSection === 1
-              ? { backgroundColor: "orange", fontSize: "larger" }
-              : { backgroundColor: "orange" }
-          }
-        >
-          Content for section 2 (RESEARCH)
-        </div>
-        <div
-          ref={sectionRefs[2]}
-          style={
-            activeSection === 2
-              ? { backgroundColor: "yellow", fontSize: "larger" }
-              : { backgroundColor: "yellow" }
-          }
-        >
-          Content for section 3 (ARTICLES)
-        </div>
+        {activeSection === 0 && (
+          <div
+            className="article-content"
+            style={{ backgroundColor: backgroundColors[0] }}
+          ></div>
+        )}
+        {activeSection === 1 && (
+          <div
+            className="article-content"
+            style={{ backgroundColor: backgroundColors[1] }}
+          >
+            <FilterTab />
+          </div>
+        )}
+        {activeSection === 2 && (
+          <div
+            className="article-content"
+            style={{ backgroundColor: backgroundColors[2] }}
+          >
+            Content for section 3 (ARTICLES)
+          </div>
+        )}
         {/* More content as needed */}
       </section>
-      
       <footer className="article-footer">footer</footer>
     </div>
   );
 }
+
 
 export default ArticlePage;
