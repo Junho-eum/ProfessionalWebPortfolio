53365df2fb87d2e74cc356dea8c5d1b55278d7b8
Junho Eum <jxe282@gmail.com>
2023-08-11 21:47:03 -0700

    Update PageTwo.jsx

--- src/pages/PageTwo.jsx
+++ src/pages/PageTwo.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import "../components/styles/Page2.css";
 import HtmlComp from "../components/3DHTML.jsx";
 import HeatmapComp from "../components/Heatmap";
-import GmmCluster from "../components/GmmCluster";
 
+
+import GmmCluster from "../components/GmmCluster";
 function Page2() {
   return (
     <div className="page">
