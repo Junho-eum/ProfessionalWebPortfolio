c90e3d948eeb6bdb2a91d847f03b346fb10d35ac
Junho Eum <jxe282@gmail.com>
2023-08-11 22:45:48 -0700

    Add files via upload

---
+++ src/components/PageSpecific/PageBaseball/3DHTML_MLB.jsx
@@ -0,0 +1,20 @@
+function HtmlComp_MLB() {
+  return (
+    <>
+      <div className="container-3dPlot left-side">
+        <p className="figure-description">
+          Use your mouse to rotate the 3D plot. Scroll to zoom in and out.
+        </p>
+        <iframe
+          src="/3-d_pytha_exp_plot_MLB.html"
+          title="3D Plot"
+          width="100%"
+          height="100%"
+          style={{ border: "none", background: "white" }}
+        />
+      </div>
+    </>
+  );
+}
+
+export default HtmlComp_MLB;
