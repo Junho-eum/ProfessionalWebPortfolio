9d3f1e6235bcae991fddd1ed2bf8ef4bb989409e
Junho Eum <jxe282@gmail.com>
2023-08-11 22:02:03 -0700

    Add files via upload

---
+++ src/components/PageSpecific/PageHome/HomePageNav.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import "../../styles/HomePageNav.css";
+
+
+function HomePageNav() {
+  return (
+    <ul className="NavulList">
+      <li className="NavList">
+        <a
+          href="https://www.linkedin.com/in/junho-eum-239409187/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          About
+        </a>
+      </li>
+      <li className="NavList">
+        <Link to="/skillsets">Skillsets</Link>
+      </li>
+      <li className="NavList">
+        <a
+          href="https://github.com/Junho-eum/Baseball_Analytics/blob/main/KBO_analytics_draft3.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default HomePageNav;
