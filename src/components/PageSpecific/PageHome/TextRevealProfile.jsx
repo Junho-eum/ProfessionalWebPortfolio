e5186316f84fbc203843e156a580c1df222a7b50
Junho Eum <jxe282@gmail.com>
2023-08-11 21:48:25 -0700

    Add files via upload

---
+++ src/components/PageSpecific/PageHome/TextRevealProfile.jsx
@@ -0,0 +1,13 @@
+// TextReveal.jsx
+import React from "react";
+import "../../styles/TextRevealProfile.css";
+
+function TextRevealProfile() {
+  return (
+    <h3 className="textReveal-reveal-profile">
+      As a creative and analytical professional,<br></br> I specialize in integrating analytics into software solutions.<br></br> With a passion for insightful analysis and exploring algorithms,<br></br> I strive to turn complex data into meaningful solutions.
+    </h3>
+  );
+}
+
+export default TextRevealProfile;
