1d15f8184728c9a3a3c7fff81a8559e66c318d57
Junho Eum <jxe282@gmail.com>
2024-01-13 20:31:02 -0500

    Update CardSliderPageFour3.jsx

--- src/components/PageSpecific/PageBaseball/CardSliderPageFour3.jsx
+++ src/components/PageSpecific/PageBaseball/CardSliderPageFour3.jsx
@@ -6,21 +6,21 @@ import '../../styles/Page4.scss'; // assuming you moved your CSS into this file
 const CardSliderPageFourThree = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const slideRef = useRef(null);
-  const slideCount = 3; // replace with the actual number of slides
+  const slideCount = 2; // replace with the actual number of slides
   const slideWidth = 1200; // match the width of each slide in your CSS
 
   const slidesData = [
     {
-      image: "2-1.png",
-      title: "Interaction Plot (Batter Features)",
+      image: "kbo_corr_heatmap2.png",
+      title: "Pairwise Correlation Plot",
       description:
-        " A comprehensive summary of these batting feature interactions is shown in the SHAP plot. Examining batting statistics I found a significant relationship between homeruns_scored and doubles with a coefficient of 032. This suggests teams skilled in hitting doubles also tend to score more home runs indicating a potential synergy between these hitting strategies.",
+        "In my analysis, I first used a pairwise correlation plot to identify and filter out highly correlated variables./nThis step helped simplify the dataset by removing redundant variables, such as certain batting and pitching statistics that showed strong correlations with each other. By doing so, I ensured a more focused and accurate SHAP analysis, enhancing the interpretability of our results.",
     },
     {
-      image: "Correlation.png",
+      image: "pitcher_interaction_summ.png",
       title: "Interaction Plot (Pitcher Features)",
       description:
-        "In the pitching statistics analysis significant interactions were identified among ERA Earned Run Average batters faced hits_x home runs allowed and walks The analysis revealed that a pitchers ERA generally increases as they face more batters likely due to the rise in scoring opportunities Also there seems to be a connection between allowing more walks and conceding more home runs emphasizing the importance of pitcher control These interactions and their impacts on the game outcomes can be seen in the SHAP interaction plots",
+        "In the SHAP plot analysis of the baseball data of KBO League, I found a significant correlation between the number of doubles and home runs a team scores, suggesting teams good at hitting doubles also tend to hit more home runs. This insight is helpful for statistical modeling, as it indicates these two variables are closely linked, and using both is redundant.\n\nSimilarly, for pitching stats, I observed that a pitcher's ERA often increases with the number of batters faced and is also affected by the number of walks and home runs allowed. This finding helps understand the relationships between these variables and guides us in choosing which ones to include in our models to avoid redundancy and improve accuracy.",
     },
     // add more slides data as needed
   ];
@@ -48,7 +48,7 @@ const CardSliderPageFourThree = () => {
 
   return (
     <div>
-      <h2 className="sliderTitle">Correlation Plots</h2>
+      <h2 className="sliderTitle">Correlation & Interaction Plots</h2>
       <div className="sliderContent" id="slider">
         <a href="#" className="control_next" onClick={moveRight}>&gt;&gt;</a>
         <a href="#" className="control_prev" onClick={moveLeft}>&lt;&lt;</a>
